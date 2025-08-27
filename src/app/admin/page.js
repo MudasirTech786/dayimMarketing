414f0d6e370e7017666ee2e37bc5af508bf67770
MudasirTech786 <m.mudasirasfi@gmail.com>
2025-08-27 16:30:29 +0500

    Initial commit with project code

--- src/app/admin/page.js
+++
@@ -1,9 +0,0 @@
-// export const dynamic = "force-dynamic";
-import React from "react";
-import AdminLayout from "./components/layouts/AdminLayout";
-
-function Home() {
-  return <AdminLayout>hello</AdminLayout>;
-}
-
-export default Home;
